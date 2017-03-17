48fff074a41d12593b4dfecf1c506701b65da5f7
fabiomacedomendes <fabiomacedomendes@gmail.com>
2017-03-16 23:06:56 -0300

    create security dir, if does not exist

--- src/codeschool/settings/security.py
+++ src/codeschool/settings/security.py
@@ -7,17 +7,22 @@ choice = random.SystemRandom().choice
 
 d = os.path.dirname
 base_dir = d(d(d(d(os.path.abspath(__file__)))))
-security_path = os.path.join(base_dir, 'security', 'secret_key.dat')
+security_path = os.path.join(base_dir, 'security')
+secrete_key_path = os.path.join(security_path, 'secret_key.dat')
 
 try:
-    with open(security_path, 'r') as F:
+    with open(secrete_key_path , 'r') as F:
         SECRET_KEY = F.read()
 
 except FileNotFoundError:
     print('Creating a new SECRET_KEY at security/secret_key.dat')
 
+    # Create directory, if it does not exist
+    if not os.path.exists(security_path):
+        os.mkdir(security_path)
+
     chars = ''.join([string.ascii_letters, string.digits, string.punctuation])
     SECRET_KEY = ''.join([choice(chars) for i in range(50)])
 
-    with open(security_path, 'w') as F:
+    with open(secrete_key_path , 'w') as F:
         SECRET_KEY = F.write(SECRET_KEY)
