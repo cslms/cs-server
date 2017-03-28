70a04d6d970f802da2ff3096522e51234c2d035f
Victor Arnaud <victorhad@gmail.com>
2017-03-28 17:04:26 -0300

    Teste de integração continua
    signed-off-by: Ygor Galeno <ygor.tgaleno@gmail.com>
    signed-off-by: João Vitor Araujo <joao18araujo@gmail.com>

--- src/codeschool/questions/numeric/tests/test_basic.py
+++ src/codeschool/questions/numeric/tests/test_basic.py
@@ -48,14 +48,14 @@ def test_less_than_tolerance(rf, question_pi, user):
     request = rf.get(question_pi.url)
     request.user = user
     submission = question_pi.submit(request, value=3.13)
-    feedback = submission.autograde()
+    feedback = submission.auto_feedback()
     assert not feedback.is_correct
 
 def test_equal_tolerance(rf, question_pi, user):
     request = rf.get(question_pi.url)
     request.user = user
     submission = question_pi.submit(request, value=3.14)
-    feedback = submission.autograde()
+    feedback = submission.auto_feedback()
     assert feedback.is_correct
     assert feedback.given_grade_pc == 100
 
@@ -63,6 +63,6 @@ def test_higher_than_tolerance(rf, question_pi, user):
     request = rf.get(question_pi.url)
     request.user = user
     submission = question_pi.submit(request, value=3.25)
-    feedback = submission.autograde()
+    feedback = submission.auto_feedback()
     assert not feedback.is_correct
     assert feedback.given_grade_pc == 0
