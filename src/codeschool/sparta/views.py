ca13f627920d12512d13273df95827f0e5411f99
Vitor Nere <vitor.nere@hotmail.com>
2017-05-03 17:49:53 -0300

    Trying use bricks with jinja

--- src/codeschool/sparta/views.py
+++ src/codeschool/sparta/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from bricks.contrib.mdl import button, div
 
 # Create your views here.
 def index(request):
-    return render(request, 'sparta/index.jinja2', {})
+    
+    ctx = {'content_body':div(button('click me', primary=True), shadow=2), 'content_title':'Hey!'}
+    return render(request, 'sparta/index.jinja2', ctx)
