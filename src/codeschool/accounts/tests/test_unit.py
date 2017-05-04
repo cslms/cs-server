73d4793ce3aac15b0843a9b7c604204f98fc39b8
Allan Pereira <pereirasallan@gmail.com>
2017-05-04 18:53:36 -0300

    Refactored tests structure to use modules test_integration and test_unit
    Signed-off-by: Allan Pereira <pereirasallan@gmail.com>
    Signed-off-by: Vitor Nere <vitor.nere@hotmail.com>

--- src/codeschool/accounts/tests/test_unit.py
+++ src/codeschool/accounts/tests/test_unit.py
@@ -36,3 +36,18 @@ def test_authenticated_auth_urls(urlchecker, user_with_profile):
         html5=False,
         login=user,
     )
+
+
+def _test_basic_urls(user, client):
+    context = {'username': user.username}
+    urls = [
+        '/auth/{username}',
+        '/auth/{username}/edit',
+        '/auth/{username}/password',
+    ]
+    client.check_url(urls, context, html5=True)
+
+
+def _test_login_with_valid_user(driver, user_with_password, password):
+    user = user_with_password
+    driver.open('/')
