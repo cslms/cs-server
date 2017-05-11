91bf1aa41cada80af8ae7a765e49a8863349a497
João Vitor Araujo <joao18araujo@gmail.com>
2017-05-11 16:18:34 -0300

    Creating regex tests to question text

--- src/codeschool/questions/text/factories.py
+++ src/codeschool/questions/text/factories.py
@@ -30,3 +30,18 @@ def make_text_questions_fuzzy(parent=None):
     parent = parent or model_reference.load('root-page')
     parent.add_child(instance=question)
     return question
+
+
+def make_regex_text_questions(parent=None):
+    question = TextQuestion(
+        title='The E-mail',
+        body=[
+            ('markdown',
+             'What is your e-mail?'),
+        ],
+        #E-mail regex
+        correct_answer='(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)',
+    )
+    parent = parent or model_reference.load('root-page')
+    parent.add_child(instance=question)
+    return question
