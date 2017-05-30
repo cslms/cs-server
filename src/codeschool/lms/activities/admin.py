903f445bf6ab6e05268f5b53d48dbfb5ace3a64d
João Vitor Araujo <joao18araujo@gmail.com>
2017-05-30 16:41:41 -0300

    Removing wildcard import in admin

--- src/codeschool/lms/activities/admin.py
+++ src/codeschool/lms/activities/admin.py
@@ -5,6 +5,3 @@ from codeschool.lms.activities.models import Progress, Submission, Feedback
 admin.site.register(Progress)
 admin.site.register(Submission)
 admin.site.register(Feedback)
-
-#FIXME: remove this once lms.listings become a real package
-from codeschool.lms.listings.admin import *
