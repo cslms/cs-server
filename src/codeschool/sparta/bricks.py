15f4379b428ee715bfbdfe05830aab7f7782a65d
Lucas Rufino <lucaslrt@outlook.com>
2017-06-02 17:19:37 -0300

    Creating test to receive members from backend.

--- src/codeschool/sparta/bricks.py
+++ src/codeschool/sparta/bricks.py
@@ -1,6 +1,7 @@
 from codeschool.bricks import navbar as _navbar, navsection, navsection_page_admin
 from bricks.html5 import a, div, h1, h2, ul, li, button
 from codeschool.bricks import card_container, simple_card, with_class
+# import members_list from back end
 
 def navbar():
     return _navbar([
@@ -14,15 +15,18 @@ def layout():
     cards = [
         simple_card('card1','bla bla bla', double=True, center=False),
         simple_card('Atividades','Atividades a fazer', icon='assignment', href='/sparta/activities'),
-        simple_card('Notas','Atividades avaliadas', icon='star', href='#'),        
+        simple_card('Notas','Atividades avaliadas', icon='star', href='#'),
     ]
 
+    # TODO: Import from backend
+    from types import SimpleNamespace
+    Member = SimpleNamespace
+    members_list = [Member(name="Goku"), Member(name="Ronaldo"), Member(name="Florentina", url="dfsdf")]
+
     b = div()[
-        ul(class_="cs-sparta__members-list",)[
-            li(a('Josefina', href='#')),
-            li(a('Xablau', href='#')),
-            li(a('Xitãozinho', href='#')),
-        ],
+        ul(class_="cs-sparta__members-list",)[[
+            li(a(member.name, href='#')) for member in members_list
+        ]],
         button(class_="button")(
                 'Avaliar membros', href='#')
     ]
