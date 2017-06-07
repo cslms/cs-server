635a4c2057175046b141420f94c2e3a69068e136
Rafael Akiyoshi <rafah94@hotmail.com>
2017-06-07 17:31:13 -0300

    adding rating members page

--- src/codeschool/sparta/urls.py
+++ src/codeschool/sparta/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='sparta_index'),
     url(r'^activities$', views.activities, name='sparta_activities'),
+    url(r'^membersrating$', views.rating, name='sparta_rating'),
 ]
--- src/codeschool/sparta/views.py
+++ src/codeschool/sparta/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from bricks.contrib.mdl import button, div
-from bricks.html5 import ul, li, a
+from bricks.html5 import ul, li, a, i
 from codeschool.bricks import navbar as _navbar, navsection
 from .bricks import navbar, layout, activities_layout
 
 # Create your views here.
 def index(request):
-    
+
     ctx = {
-        'main':layout(), 
+        'main':layout(),
         'navbar':navbar(),
     }
     return render(request, 'sparta/index.jinja2', ctx)
@@ -20,3 +20,27 @@ def activities(request):
         'content_body':activities_layout(),
     }
     return render(request, 'sparta/activities.jinja2', ctx)
+
+def rating(request):
+
+    ctx = {
+        'content_title':'Membros',
+        'content_body': ul(class_="demo-list-icon mdl-list")[
+            li(class_="mdl-list__item")[
+            i(class_="material-icons mdl-list__item-icon")['person'],
+            (a('Goku', href='#')),
+            a(class_="mdl-list__item-secondary-action")(href='#')[i(class_="material-icons")['star']]
+            ],
+            li(class_="mdl-list__item")[
+            i(class_="material-icons mdl-list__item-icon")['person'],
+            (a('Ronaldo', href='#')),
+            a(class_="mdl-list__item-secondary-action")(href='#')[i(class_="material-icons")['star']]
+            ],
+            li(class_="mdl-list__item")[
+            i(class_="material-icons mdl-list__item-icon")['person'],
+            (a('Florentina', href='#')),
+            a(class_="mdl-list__item-secondary-action")(href='#')[i(class_="material-icons")['star']]
+            ]
+        ]
+    }
+    return render(request, 'sparta/rating.jinja2', ctx)
