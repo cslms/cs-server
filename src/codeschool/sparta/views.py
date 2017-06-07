53cd295fc445f06f3c1dd627c2cda83bdb0aaaab
Rafael Akiyoshi <rafah94@hotmail.com>
2017-06-07 17:57:03 -0300

    adding dropdown for rating

--- src/codeschool/sparta/views.py
+++ src/codeschool/sparta/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from bricks.contrib.mdl import button, div
-from bricks.html5 import ul, li, a, i
+from bricks.html5 import ul, li, a, i, select, option
 from codeschool.bricks import navbar as _navbar, navsection
 from .bricks import navbar, layout, activities_layout
 
@@ -8,16 +8,16 @@ from .bricks import navbar, layout, activities_layout
 def index(request):
 
     ctx = {
-        'main':layout(),
-        'navbar':navbar(),
+    'main':layout(),
+    'navbar':navbar(),
     }
     return render(request, 'sparta/index.jinja2', ctx)
 
 def activities(request):
 
     ctx = {
-        'content_title':'Atividades',
-        'content_body':activities_layout(),
+    'content_title':'Atividades',
+    'content_body':activities_layout(),
     }
     return render(request, 'sparta/activities.jinja2', ctx)
 
@@ -26,21 +26,27 @@ def rating(request):
     ctx = {
         'content_title':'Membros',
         'content_body': ul(class_="demo-list-icon mdl-list")[
-            li(class_="mdl-list__item")[
+        li(class_="mdl-list__item")[
             i(class_="material-icons mdl-list__item-icon")['person'],
             (a('Goku', href='#')),
-            a(class_="mdl-list__item-secondary-action")(href='#')[i(class_="material-icons")['star']]
-            ],
-            li(class_="mdl-list__item")[
+            a(class_="mdl-list__item-secondary-action")(href='#')[i(class_="material-icons")['star']],
+            select(class_="mdl-select__input")[
+                option('1'),
+                option('2'),
+                option('3'),
+                option('4'),
+            ]
+        ],
+        li(class_="mdl-list__item")[
             i(class_="material-icons mdl-list__item-icon")['person'],
             (a('Ronaldo', href='#')),
             a(class_="mdl-list__item-secondary-action")(href='#')[i(class_="material-icons")['star']]
-            ],
-            li(class_="mdl-list__item")[
+        ],
+        li(class_="mdl-list__item")[
             i(class_="material-icons mdl-list__item-icon")['person'],
             (a('Florentina', href='#')),
             a(class_="mdl-list__item-secondary-action")(href='#')[i(class_="material-icons")['star']]
-            ]
         ]
+    ]
     }
     return render(request, 'sparta/rating.jinja2', ctx)
