a8c0ba37121f332a859a974dbd28d8bddd16d49d
Allan Pereira <pereirasallan@gmail.com>
2017-06-22 18:50:54 -0300

    refatored tests in module text question to adopt testing framework

--- src/codeschool/questions/text/tests/test_text.py
+++ src/codeschool/questions/text/tests/test_text.py
@@ -1,25 +1,17 @@
-import pytest
+from codeschool.questions.text.factories import make_text_question, \
+    make_regex_text_question
+from .test_db import question_first, question_regex
 
-from codeschool.questions.text.factories import make_text_questions, \
-    make_text_questions_fuzzy, make_regex_text_questions
-
-pytestmark = pytest.mark.integration
-
-@pytest.fixture
-def question_first(db):
-    return make_text_questions()
-
-@pytest.fixture
-def question_regex(db):
-    return make_regex_text_questions()
 
 def test_make_text_question(db, user):
-    question = make_text_questions()
-    assert question.title == 'The Answer'
+    question = make_text_question()
+    assert question.title == 'The textual answer for everything'
+
 
 def test_make_regex_text_question(db, user):
-    question = make_regex_text_questions()
+    question = make_regex_text_question()
     assert question.title == 'The E-mail'
+
 
 def test_correct_submission_text_question(rf, question_first, user):
     request = rf.get(question_first.url)
@@ -36,12 +28,14 @@ def test_incorrect_submission_text_question(rf, question_first, user):
     feedback = submission.auto_feedback()
     assert not feedback.is_correct
 
+
 def test_correct_uppercase_submition_text_question(rf, question_first, user):
     request = rf.get(question_first.url)
     request.user = user
     submission = question_first.submit(request, value='QUARENTA E TRÊS')
     feedback = submission.auto_feedback()
     assert feedback.is_correct
+
 
 def test_incorrect_uppercase_submition_text_question(rf, question_first, user):
     request = rf.get(question_first.url)
@@ -50,12 +44,14 @@ def test_incorrect_uppercase_submition_text_question(rf, question_first, user):
     feedback = submission.auto_feedback()
     assert not feedback.is_correct
 
+
 def test_correct_lowercase_submition_text_question(rf, question_first, user):
     request = rf.get(question_first.url)
     request.user = user
     submission = question_first.submit(request, value='quarenta e três')
     feedback = submission.auto_feedback()
     assert feedback.is_correct
+
 
 def test_incorrect_lowercase_submition_text_question(rf, question_first, user):
     request = rf.get(question_first.url)
@@ -64,12 +60,14 @@ def test_incorrect_lowercase_submition_text_question(rf, question_first, user):
     feedback = submission.auto_feedback()
     assert not feedback.is_correct
 
+
 def test_correct_accentuated_submition_text_question(rf, question_first, user):
     request = rf.get(question_first.url)
     request.user = user
     submission = question_first.submit(request, value='quarenta e tres')
     feedback = submission.auto_feedback()
     assert feedback.is_correct
+
 
 def test_incorrect_accentuated_submition_text_question(rf, question_first, user):
     request = rf.get(question_first.url)
@@ -78,16 +76,10 @@ def test_incorrect_accentuated_submition_text_question(rf, question_first, user)
     feedback = submission.auto_feedback()
     assert not feedback.is_correct
 
+
 def test_correct_regex_submition_text_question(rf, question_regex, user):
     request = rf.get(question_regex.url)
     request.user = user
     submission = question_regex.submit(request, value='loremipsum@email.com')
     feedback = submission.auto_feedback()
     assert feedback.is_correct
-
-def test_incorrect_regex_submition_text_question(rf, question_regex, user):
-    request = rf.get(question_regex.url)
-    request.user = user
-    submission = question_regex.submit(request, value='emailemail.com')
-    feedback = submission.auto_feedback()
-    assert not feedback.is_correct
