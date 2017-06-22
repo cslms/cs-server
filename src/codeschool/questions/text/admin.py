aeefaac19cf498ba31f282bb8591855d86e90ef8
Allan Pereira <pereirasallan@gmail.com>
2017-06-22 18:33:13 -0300

    Adapted text questions to admin structure

--- src/codeschool/questions/text/admin.py
+++ src/codeschool/questions/text/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.utils.text import ugettext_lazy as _
 
-# Register your models here.
+from codeschool import panels
+from codeschool.questions.admin import QuestionAdmin
+from . import models
+
+admin.site.register(models.TextQuestion)
+admin.site.register(models.TextProgress)
+admin.site.register(models.TextSubmission)
+admin.site.register(models.TextFeedback)
+
+
+class TextQuestionAdmin(QuestionAdmin):
+
+    class Meta:
+        model = models.TextQuestion
+
+    content_panels = QuestionAdmin.content_panels + [
+        panels.MultiFieldPanel([
+            panels.FieldPanel('correct_answer'),
+            panels.FieldPanel('label'),
+            panels.FieldPanel('help_text'),
+        ], heading=_('Text value'))
+    ]
+    content_panels.append(content_panels.pop(-2))
