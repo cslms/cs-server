900a191cee4d83c2d3f08fbee2177e20a9429f2c
Lucas Amoêdo <lucas_advc@outlook.com>
2017-07-05 17:16:14 -0300

    Refactor sparta view
    Signed-off-by: Lucas Amoêdo <lucas_advc@outlook.com>
    Signed-off-by: Vitor Nere <vitor.nere@hotmail.com>

--- src/codeschool/sparta/bricks.py
+++ src/codeschool/sparta/bricks.py
@@ -1,46 +1,100 @@
 from codeschool.bricks import navbar as _navbar, navsection, navsection_page_admin
-from bricks.html5 import a, div, h1, h2, ul, li, button
+from bricks.html5 import ul, li, a, i, select, option, input, table, tbody, thead, th, td, tr, div, h2, button
 from codeschool.bricks import card_container, simple_card, with_class
-from simple_search import search_filter
+from django.urls import reverse
 # import members_list from back end
+
 
 def navbar():
     return _navbar([
         navsection('Menu',
-            [a('Home Sparta', href='#'),
-            a('Mensagens', href='#')
-        ])])
+                   [a('Home Sparta', href='#'),
+                    a('Mensagens', href='#')
+                    ])])
+
 
 @with_class('cs-sparta')
 def layout():
     cards = [
-        simple_card('card1','bla bla bla', double=True, center=False),
-        simple_card('Atividades','Atividades a fazer', icon='assignment', href='/sparta/activities'),
-        simple_card('Notas','Atividades avaliadas', icon='star', href='#'),
+        simple_card('card1', 'bla bla bla', double=True, center=False),
+        simple_card('Atividades', 'Atividades a fazer',
+                    icon='assignment', href='/sparta/activities'),
+        simple_card('Notas', 'Atividades avaliadas', icon='star', href='#'),
     ]
 
     # TODO: Import from backend
     from types import SimpleNamespace
     Member = SimpleNamespace
-    members_list = [Member(name="Goku"), Member(name="Ronaldo"), Member(name="Florentina", url="dfsdf")]
+    members_list = [Member(name="Goku"), Member(
+        name="Ronaldo"), Member(name="Florentina", url="dfsdf")]
 
     b = div()[
         ul(class_="cs-sparta__members-list",)[[
             li(a(member.name, href='#')) for member in members_list
         ]],
-        button(class_="button")(
-                'Avaliar membros', href='#')
+        a(class_="button", href=reverse('sparta_rating'))[
+            ('Avaliar')
+        ]
     ]
     return card_container(cards, title='Membros', description=b)
+
 
 def activities_layout():
 
     # TODO: Import from backend
     from types import SimpleNamespace
     Activity = SimpleNamespace
-    activities_list = [Activity(name="Atividade 1"), Activity(name="Atividade 2"), Activity(name="Atividade 3", url="dfsdf")]
+    activities_list = [Activity(name="Atividade 1"), Activity(
+        name="Atividade 2"), Activity(name="Atividade 3", url="dfsdf")]
 
     # activities_list = activity_filter()
 
-    cards = [ simple_card(activity.name, double=True, center=False) for activity in activities_list ]
+    cards = [simple_card(activity.name, double=True, center=False)
+             for activity in activities_list]
     return card_container(cards)
+
+
+def rating_layout():
+    layout = ul(class_="demo-list-icon mdl-list")[
+        table(class_="mdl-data-table mdl-js-data-table mdl-shadow--2dp")[
+            div(class_="mdl-tooltip", for_="tt1")['Já Avaliado'],
+            thead()[
+                tr()[
+                    th(class_="mdl-data-table__cell--non-numeric")['Aluno'],
+                    th()['Avaliações'],
+                    th()['Nota'],
+                    th()['Avaliar'],
+                ]
+            ],
+            tbody()[
+                tr()[
+                    td(class_="mdl-data-table__cell--non-numeric")[
+                        'Goku da Silva Mendes'],
+                    td()['3 Aluno(s)'],
+                    td()['10'],
+                    td()[
+                        i(class_="material-icons", id='tt1')['done'],
+                    ],
+                ],
+                tr()[
+                    td(class_="mdl-data-table__cell--non-numeric")[
+                        'Ronaldo Andrade Souza'],
+                    td()['0 Aluno(s)'],
+                    td()['0'],
+                    td()[
+                        i(class_="material-icons")['mode_edit'],
+                    ]
+                ],
+                tr()[
+                    td(class_="mdl-data-table__cell--non-numeric")[
+                        'Florentina de Jesus'],
+                    td()['0 Aluno(s)'],
+                    td()['0'],
+                    td()[
+                        i(class_="material-icons")['mode_edit'],
+                    ]
+                ],
+            ],
+        ],
+    ]
+    return layout
--- src/codeschool/sparta/views.py
+++ src/codeschool/sparta/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from bricks.contrib.mdl import button, div
-from bricks.html5 import ul, li, a, i, select, option, input, table, tbody, thead, th, td, tr
 from codeschool.bricks import navbar as _navbar, navsection
-from .bricks import navbar, layout, activities_layout
+from .bricks import navbar, layout, activities_layout, rating_layout
 
 # Create your views here.
 def index(request):
@@ -25,44 +24,6 @@ def rating(request):
 
     ctx = {
         'content_title':'Avaliação dos Membros',
-        'content_body': ul(class_="demo-list-icon mdl-list")[
-        table(class_="mdl-data-table mdl-js-data-table mdl-shadow--2dp")[
-        div(class_="mdl-tooltip", for_="tt1")['Já Avaliado'],
-          thead()[
-            tr()[
-              th(class_="mdl-data-table__cell--non-numeric")['Aluno'],
-              th()['Avaliações'],
-              th()['Nota'],
-              th()['Avaliar'],
-            ]
-          ],
-          tbody()[
-            tr()[
-              td(class_="mdl-data-table__cell--non-numeric")['Goku da Silva Mendes'],
-              td()['3 Aluno(s)'],
-              td()['10'],
-             td()[
-               i(class_="material-icons", id='tt1')['done'],
-               ],
-            ],
-            tr()[
-              td(class_="mdl-data-table__cell--non-numeric")['Ronaldo Andrade Souza'],
-              td()['0 Aluno(s)'],
-              td()['0'],
-              td()[
-                i(class_="material-icons")['mode_edit'],
-                ]
-            ],
-            tr()[
-              td(class_="mdl-data-table__cell--non-numeric")['Florentina de Jesus'],
-              td()['0 Aluno(s)'],
-              td()['0'],
-              td()[
-                i(class_="material-icons")['mode_edit'],
-                ]
-            ],
-          ],
-        ],
-    ]
+        'content_body': rating_layout()
     }
     return render(request, 'sparta/rating.jinja2', ctx)
