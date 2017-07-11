9301d36e09364c87f355c767a91729ce4c1a1e26
jonnatas <jonatas_lenon@hotmail.com>
2017-07-11 20:38:21 -0300

    add form of detail

--- src/codeschool/blog/urls.py
+++ src/codeschool/blog/urls.py
@@ -11,8 +11,6 @@ urlpatterns = [
     url(r'^post/new/$', views.post_new, name='post_new'),
     #url(r'^drafts/$', views.post_draft_list, name='postdraftlist'),
     url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='postremove'),
-    url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='postpublish'),
     url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='addcomment'),
-    url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
 	url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
 ]
